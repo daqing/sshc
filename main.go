cc4a10dc94bf5028ded1e36c55540236fa7b34a0
David Zhang <v8os.com@gmail.com>
2021-07-27 00:15:26 +0800

    设置host目录

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -10,6 +11,7 @@ import (
 )
 
 type HostInfo struct {
+	Key      string
 	IP       string
 	User     string
 	Password string
@@ -18,7 +20,7 @@ type HostInfo struct {
 func run(client *goph.Client, cmd string) {
 	out, err := client.Run(cmd)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	fmt.Printf("%s", string(out))
@@ -38,15 +40,25 @@ func down(client *goph.Client, dest, src string) {
 	}
 }
 
+// sshc -c $HOME/hosts v8os run ls
+// sshc -c $HOME/hosts v8os cp "/tmp/abc.txt" "/tmp/foo.txt"
+// sshc -c $HOME/hosts v8os down "/tmp/foo.txt" "/tmp/demo.txt"
 func main() {
-	if len(os.Args) < 3 {
+	if len(os.Args) < 5 {
 		fmt.Printf("usage: %s [host] [action] [arg1 arg2 arg3 ...]\n", os.Args[0])
 		return
 	}
 
-	host := os.Args[1]
+	confDir := flag.String("c", "", "config dir to load [host].toml")
+	flag.Parse()
 
-	content, err := os.ReadFile(fmt.Sprintf("config/%s.toml", host))
+	if len(*confDir) == 0 {
+		log.Fatal("Please provide conf dir via -c [path/to/dir]")
+	}
+
+	host := os.Args[3]
+
+	content, err := os.ReadFile(fmt.Sprintf("%s/%s.toml", *confDir, host))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -57,34 +69,48 @@ func main() {
 		log.Fatal(err)
 	}
 
-	client, err := goph.New(hostInfo.User, hostInfo.IP, goph.Password(hostInfo.Password))
+	var client *goph.Client
+	var auth goph.Auth
+
+	if len(hostInfo.Key) > 0 {
+		auth, err = goph.Key(hostInfo.Key, "")
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		auth = goph.Password(hostInfo.Password)
+	}
+
+	client, err = goph.New(hostInfo.User, hostInfo.IP, auth)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer client.Close()
 
-	action := os.Args[2]
+	action := os.Args[4]
 	switch action {
 	case "run":
-		if len(os.Args) == 3 {
+		if len(os.Args) == 5 {
 			fmt.Println("please provide command to run")
 			return
 		}
 
-		run(client, os.Args[3])
-	case "cp":
-		if len(os.Args) < 5 {
+		run(client, os.Args[5])
+	case "scp", "cp":
+		if len(os.Args) == 5 {
 			fmt.Printf("usage: %s cp [src] [dest]\n", os.Args[0])
 			return
 		}
 
-		cp(client, os.Args[3], os.Args[4])
+		cp(client, os.Args[5], os.Args[6])
 	case "down":
-		if len(os.Args) < 5 {
+		if len(os.Args) == 5 {
 			fmt.Printf("usage: %s down [dest] [src]\n", os.Args[0])
 			return
 		}
 
-		down(client, os.Args[3], os.Args[4])
+		down(client, os.Args[5], os.Args[6])
+	default:
+		log.Fatal("unknown action: " + action)
 	}
 }
